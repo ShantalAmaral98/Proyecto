ada94667445176c95df0778333f0380afcc6ce64
Shantal Amaral <161666651+ShantalAmaral98@users.noreply.github.com>
2024-03-04 18:50:46 -0800

    Update README.php

--- README.php
+++ README.php
@@ -3,9 +3,14 @@ echo "API"
   //Crear objeto JSON
   //Crear un array 
   $Objeto = [
+  //Shantal 
+  "Nombre" => "Lizzie",
+  "Color de carro" => "Negro",
+  "Atributo2" => "String",
+  "Atributo4" => "Int",
+  "Atributo5" => "Float",
   
-  "Atributo1" => "Lizzie",
-  "Atributo2" => "Negro",
+  ] 
   
 
 
