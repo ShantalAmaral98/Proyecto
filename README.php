b389bcaaa31db62b9cf355e5e302880e8a476cc5
ibarraMi <162224425+ibarraMi@users.noreply.github.com>
2024-03-04 18:53:08 -0800

    Update README.php

--- README.php
+++ README.php
@@ -13,5 +13,31 @@ echo "API"
   ] 
   
 
+<?php
+echo "API"
+  //Crear objeto JSON
+  //Crear un array 
 
+  //MIGUEL
+  $RayoMcqueen = [
+  "color" => "rojo brillante",
+  "numero de participante" => "9",
+  "caracter" => "amistoso y terco",
+  "Diseño" => "",
+  "atributo5" => "String",
+  "atributo6" => "Boleano",
+  "atributo7" => "String",
+  "atributo8" => "String",
+  "atributo9" => "Float",
+  "atributo10" => "int",
+  "atributo11" => "String",
+  "atributo12" => "int",
+  "atributo13" => "int",
+  "atributo14" => "int",
+  "atributo15" => "int",
+  "atributo16"=> "int",
+  "atributo17" => "int",
+  "atributo18" => "int",
+  "atributo19" => "int",
+  "atributo20" => "int",
 
